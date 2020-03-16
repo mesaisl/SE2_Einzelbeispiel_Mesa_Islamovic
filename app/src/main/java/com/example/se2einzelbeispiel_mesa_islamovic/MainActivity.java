d6ade6560d817b370a07f8fac6c1e14f4264215c
mesai <mesaislamovic@hotmail.com>
2020-03-16 11:08:52 +0100

    activity_main.xml implemented. App designed

---
+++ app/src/main/java/com/example/se2einzelbeispiel_mesa_islamovic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.se2einzelbeispiel_mesa_islamovic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
